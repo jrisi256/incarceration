0963ef46711959a01d980ad20ec5c939169d5e5e
jrisi256 <jrisi256@gmail.com>
2023-09-12 15:30:02 -0400

    Finished 2021.

--- health_data/src/checklist.md
+++ health_data/src/checklist.md
@@ -21,16 +21,18 @@
 * I recalculated *liquor_store_density* in 2010 to be per 100,000 people rather than 10,000 people. Additionally, I recalculated it in 2011 as well because the creators of the data originally rounded their value to the nearest whole number.
 * In 2012, there are duplicate columns for *access_to_healthy_foods*, and I remove those columns.
 * From 2010-2012, lead poisoned children was named *lead_poisoned_children_(wi)*. Starting in 2013, the variable name changed to *lead_poisoned_children*. I update the values from 2010-2012 to reflect the change in the variable name.
-* From 2016-2020, *drinking_water_violations* changes from a percentage variable (% of people exposed to a drinking water violation) to a binary variable (county was or was not exposed to a drinking water violation). I change the name of this variable to *drinking_water_violations_bin* to reflect the new meaning of the variable.
+* From 2011-2019, I rename *%_non-hispanic_african_american* to *%_non-hispanic_black* for consistency purposes because newer releases of the data use Black instead of African-American.
+* From 2016-2021, *drinking_water_violations* changes from a percentage variable (% of people exposed to a drinking water violation) to a binary variable (county was or was not exposed to a drinking water violation). I change the name of this variable to *drinking_water_violations_bin* to reflect the new meaning of the variable.
 * From 2011-2021, I change *ratio_of_population_to_primary_care* to *primary_care_physicians*, and I change the stem value to *ratio*.
 * From 2011-2021, I change *ratio_of_population_mental_health* to *mental_health_providers*, and I change the stem value to *ratio*.
 * From 2012-2021, I change *ratio_of_population_to_dentists* to *dentists*, and I change the stem value to *ratio*.
-* From 2014-2020, I change *ratio_of_population_to_primary_care_providers_other_than_physicians* to *other_primary_care_providers*, and I change the stem value to *ratio*.
-* From 2019-2020, I change the stem value for *percentage_of_households_with_high_housing_costs* to *raw_value*.
-* From 2019-2020, I change the stem value for *percentage_of_households_with_overcrowding* to *raw_value*.
-* From 2019-2020, I change the stem value for *percentage_of_households_with_lack_of_kitchen_or_plumbing* to *raw_value*.
+* From 2014-2021, I change *ratio_of_population_to_primary_care_providers_other_than_physicians* to *other_primary_care_providers*, and I change the stem value to *ratio*.
+* From 2019-2021, I change the stem value for *percentage_of_households_with_high_housing_costs* to *raw_value*.
+* From 2019-2021, I change the stem value for *percentage_of_households_with_overcrowding* to *raw_value*.
+* From 2019-2021, I change the stem value for *percentage_of_households_with_lack_of_kitchen_or_plumbing* to *raw_value*.
 * From 2019-2021, I recalculate *preventable_hospital_stays* to be per 1,000 people rather than per 100,000 people to keep it consistent with how the variable was measured in previous years.
+* From 2019-2021, I rename *mammography_screening* to *mammography_screening_65_74*. From 2011-2018, I rename *mammography_screening* to *mammography_screening_67_69*. The name change is due to changes in how mammography screening is measured which rendered observations from 2018 and earlier incomparable to observations from 2019 and later.
 * From 2020-2021, I rename *lbw* (which is the name of the unreliable indicator flag for *low_birthweight*) to *low_birthweight*.
-* From 2020-2020, I change the stem value for *crude_suicide* to *raw_value*.
-* From 2020-2020, I change the stem value for *number_of_juvenile_delinquency_cases_formally_processed_by_a_juvenile_court* to *raw_value*.
-* From 2020-2020, I change the stem value for *number_of_informally_handled_juvenile_delinquency_cases* to *raw_value*.
+* From 2020-2021, I change the stem value for *crude_suicide* to *raw_value*.
+* From 2020-2021, I change the stem value for *number_of_juvenile_delinquency_cases_formally_processed_by_a_juvenile_court* to *raw_value*.
+* From 2020-2021, I change the stem value for *number_of_informally_handled_juvenile_delinquency_cases* to *raw_value*.
--- health_data/src/read-data.R
+++ health_data/src/read-data.R
@@ -77,16 +77,19 @@ county_level <- health_data_dfs %>% filter(str_sub(full_fips, 3, 5) != "000")
 ########################## Change income inequality to gini coefficient in 2010.
 ############# Change lead poisoned children from 2010-2012 to match later years.
 ########### Change primary_care_provider_rate stems and variable names for 2010.
-####### Change stem values for severe housing problems variables from 2019-2020.
+## I rename mammography_screening to mammography_screening_67_69 from 2011-2018.
+# Change %_non-hispanic_african_american to %_non-hispanic_black from 2011-2019.
+####### Change stem values for severe housing problems variables from 2019-2021.
 ### Recalculate preventable hospital stays to be per 1000 people from 2019-2021.
-######## Change water violations name to reflect its new meaning from 2016-2020.
+######## Change water violations name to reflect its new meaning from 2016-2021.
 ######### Change primary_care_physician stems and variable names from 2011-2021.
 ######### Change mental_health_provider stems and variable names from 2011-2021.
-### Change other_primary_care_providers stems and variable names from 2014-2020.
+### Change other_primary_care_providers stems and variable names from 2014-2021.
 ####################### Change dentists stems and variable names from 2012-2021.
 ################################## Rename lbw to low_birthweight from 2020-2021.
-################# Change the stem to raw_value for crude_suicide from 2020-2020.
-###### Change the stems/names for formal/informal juvenile cases from 2020-2020.
+################# Change the stem to raw_value for crude_suicide from 2020-2021.
+###### Change the stems/names for formal/informal juvenile cases from 2020-2021.
+## I rename mammography_screening to mammography_screening_65_74 From 2019-2021.
 county_level_harmonize <-
   county_level %>%
   mutate(
@@ -104,7 +107,7 @@ county_level_harmonize <-
       ),
     variable =
       if_else(
-        release_year %in% c(2010, 2011, 2012) & variable == "lead_poisoned_children_(wi)",
+        release_year %in% 2010:2012 & variable == "lead_poisoned_children_(wi)",
         "lead_poisoned_children",
         variable
       ),
@@ -134,7 +137,7 @@ county_level_harmonize <-
       ),
     stem = 
       if_else(
-        release_year %in% c(2019, 2020) &
+        release_year %in% 2019:2021 &
           variable %in% c(
             "percentage_of_households_with_high_housing",
             "percentage_of_households_with",
@@ -144,110 +147,116 @@ county_level_harmonize <-
         stem
       ),
     variable = 
+      case_when(
+        release_year %in% 2011:2018 & variable == "mammography_screening" ~ "mammography_screening_67_69",
+        release_year %in% 2019:2021 & variable == "mammography_screening" ~ "mammography_screening_65_74",
+        T ~ variable
+      ),
+    variable = 
       if_else(
-        release_year %in% c(2019, 2020) & variable == "percentage_of_households_with_high_housing",
+        release_year %in% 2019:2021 & variable == "percentage_of_households_with_high_housing",
         "percentage_of_households_with_high_housing_costs",
         variable
       ),
     variable =
       if_else(
-        release_year %in% c(2019, 2020) & variable == "percentage_of_households_with",
+        release_year %in% 2019:2021 & variable == "percentage_of_households_with",
         "percentage_of_households_with_overcrowding",
         variable
       ),
     values =
       if_else(
-        release_year %in% c(2019, 2020, 2021) & variable == "preventable_hospital_stays" & stem == "raw_value",
+        release_year %in% 2019:2021 & variable == "preventable_hospital_stays" & stem == "raw_value",
         values / 100,
         values
       ),
     variable = 
       if_else(
-        release_year %in% c(2016, 2017, 2018, 2019, 2020) & variable == "drinking_water_violations",
+        release_year %in% 2016:2021 & variable == "drinking_water_violations",
         "drinking_water_violations_bin",
         variable
       ),
     stem =
       if_else(
-        release_year %in% c(2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021) & variable == "ratio_of_population_to_primary_care",
+        release_year %in% 2011:2021 & variable == "ratio_of_population_to_primary_care",
         "ratio",
         stem
     ),
     variable = 
       if_else(
-        release_year %in% c(2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021) & variable == "ratio_of_population_to_primary_care",
+        release_year %in% 2011:2021 & variable == "ratio_of_population_to_primary_care",
         "primary_care_physicians",
         variable
       ),
     stem =
       if_else(
-        release_year %in% c(2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021) & variable == "ratio_of_population_to_mental_health",
+        release_year %in% 2011:2021 & variable == "ratio_of_population_to_mental_health",
         "ratio",
         stem
       ),
     variable =
       if_else(
-        release_year %in% c(2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021) & variable == "ratio_of_population_to_mental_health",
+        release_year %in% 2011:2021 & variable == "ratio_of_population_to_mental_health",
         "mental_health_providers",
         variable
       ),
     stem =
       if_else(
-        release_year %in% c(2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021) & variable == "ratio_of_population_to",
+        release_year %in% 2012:2021 & variable == "ratio_of_population_to",
         "ratio",
         stem
       ),
     variable =
       if_else(
-        release_year %in% c(2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021) & variable == "ratio_of_population_to",
+        release_year %in% 2012:2021 & variable == "ratio_of_population_to",
         "dentists",
         variable
         ),
     stem = 
       if_else(
-        release_year %in% c(2014, 2015, 2016, 2017, 2018, 2019, 2020) & variable == "ratio_of_population_to_primary_care_providers_other_than",
+        release_year %in% 2014:2021 & variable == "ratio_of_population_to_primary_care_providers_other_than",
         "ratio",
         stem
       ),
     variable =
       if_else(
-        release_year %in% c(2014, 2015, 2016, 2017, 2018, 2019, 2020) & variable == "ratio_of_population_to_primary_care_providers_other_than",
+        release_year %in% 2014:2021 & variable == "ratio_of_population_to_primary_care_providers_other_than",
         "other_primary_care_providers",
         variable
       ),
     variable =
       if_else(
-        release_year %in% c(2020, 2021) & variable == "lbw",
+        release_year %in% 2020:2021 & variable == "lbw",
         "low_birthweight",
         variable
       ),
     stem =
       if_else(
-        release_year %in% c(2020) & variable == "crude_suicide",
+        release_year %in% 2020:2021 & variable == "crude_suicide",
         "raw_value",
         stem
       ),
     stem =
       if_else(
-        release_year %in% c(2020) & variable == "number_of_juvenile_delinquency_cases_formally_processed_by_a_juvenile",
+        release_year %in% 2020:2021 & variable == "number_of_juvenile_delinquency_cases_formally_processed_by_a_juvenile",
         "raw_value",
         stem
       ),
     variable =
       if_else(
-        release_year %in% c(2020) & variable == "number_of_juvenile_delinquency_cases_formally_processed_by_a_juvenile",
+        release_year %in% 2020:2021 & variable == "number_of_juvenile_delinquency_cases_formally_processed_by_a_juvenile",
         "number_of_juvenile_delinquency_cases_formally_processed_by_a_juvenile_court",
         variable
       ),
     stem =
       if_else(
-        release_year %in% c(2020) & variable == "number_of_informally_handled_juvenile_delinquency",
+        release_year %in% 2020:2021 & variable == "number_of_informally_handled_juvenile_delinquency",
         "raw_value",
         stem
       ),
     variable =
       if_else(
-        release_year %in% c(2020) & variable == "number_of_informally_handled_juvenile_delinquency",
+        release_year %in% 2020:2021 & variable == "number_of_informally_handled_juvenile_delinquency",
         "number_of_informally_handled_juvenile_delinquency_cases",
         variable
       )
@@ -324,8 +333,10 @@ county_level_harmonize <-
   filter(release_year != 2012 | variable != "access_to_healthy_foods") %>%
   bind_rows(health_foods_harmonize_2012)
 
+a2020 <-
+  county_level_harmonize %>%
+  filter(release_year == 2020, full_fips == "01001")
+
 a2021 <-
   county_level_harmonize %>%
   filter(release_year == 2021, full_fips == "01001")
-
-# TO DO FOR TOMORROW FIX MAMMOGRAPHY SCREENING, DISJUNCTURE IN FROM 2018-2019
