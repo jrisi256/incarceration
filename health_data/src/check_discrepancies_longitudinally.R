06f132cb5e1223fdbeaddf40a7135337d97af245
jrisi256 <jrisi256@gmail.com>
2023-10-03 15:34:27 -0400

    Tired. Read a lot about decomposition of variance. Made some progress on fixing my dictionary.

--- health_data/src/check_discrepancies_longitudinally.R
+++ health_data/src/check_discrepancies_longitudinally.R
@@ -1,6 +1,7 @@
 library(here)
 library(dplyr)
 library(readr)
+library(purrr)
 library(ggplot2)
 
 county_harmonize <-
@@ -20,12 +21,15 @@ wi_fl_ny_only <-
   filter(notes %in% discard_vars) %>%
   distinct(variable, .keep_all = T)
 
-# Nation level
-nation_level_longitudinal <-
+clean <-
   county_harmonize %>%
   filter(stem == "raw_value", is.na(race)) %>%
   select(-race) %>%
-  filter(!(variable %in% wi_fl_ny_only$variable)) %>%
+  filter(!(variable %in% wi_fl_ny_only$variable))
+
+# Nation level
+nation_level_longitudinal <-
+  clean %>%
   group_by(variable, release_year) %>%
   summarise(mean_value = mean(values, na.rm = T)) %>%
   mutate(standardized_mean_values = scale(mean_value)[, 1]) %>%
@@ -44,5 +48,43 @@ ggplot(
   theme(legend.position = "none") +
   facet_wrap(~variable)
 
-a <- county_level %>% filter(str_detect(variable, "african|c_black")) %>% distinct(variable, release_year)
-a2 <- county_harmonize %>% filter(full_fips == "01001", variable == "premature_death")
+# State level
+state_level_trends <-
+  map(unique(clean$variable),
+      function(col_name, df) {
+        df %>%
+          filter(variable == col_name) %>%
+          group_by(state_fips, variable, release_year) %>%
+          summarise(mean_value = mean(values, na.rm = T)) %>%
+          mutate(standardized_mean_values = scale(mean_value)[, 1]) %>%
+          ungroup()
+      },
+      df = clean)
+
+names(state_level_trends) <- unique(clean$variable)
+
+ggplot(state_level_trends$poor_physical_health_days, aes(x = release_year, y = standardized_mean_values)) +
+  geom_point(aes(color = state_fips)) +
+  geom_line(aes(color = state_fips, group = state_fips)) +
+  theme_bw()
+
+# county level
+county_level_trends <-
+  map(unique(clean$variable),
+      function(col_name, df) {
+        df %>%
+          filter(variable == col_name) %>%
+          group_by(full_fips, variable, release_year) %>%
+          summarise(mean_value = mean(values, na.rm = T)) %>%
+          mutate(standardized_mean_values = scale(mean_value)[, 1]) %>%
+          ungroup()
+      },
+      df = clean)
+
+names(county_level_trends) <- unique(clean$variable)
+
+ggplot(county_level_trends$premature_death, aes(x = release_year, y = standardized_mean_values)) +
+  geom_point(aes(color = full_fips)) +
+  geom_line(aes(color = full_fips, group = full_fips)) +
+  theme_bw() +
+  theme(legend.position = "none")
